4e71decdd9b33837f3929de39317d9b26868e9c3
Jissmon Jose <37608527+jisshub@users.noreply.github.com>
2020-12-24 16:36:06 +0530

    sadsad

---
+++ my-app/src/AddNinja.js
@@ -0,0 +1,31 @@
+import React,{ Component } from 'react';
+
+export default class AddNinja extends Component{
+    state = {
+        name: null,
+        age: null,
+        belt: null
+    }
+    handleChange = (e) =>{
+        this.setState({[e.target.id]: e.target.value});
+    }
+    formSubmit=(e)=>{
+        e.preventDefault();
+        console.log(this.state);
+    }
+    render(){
+        return(
+            <div>
+                <form onSubmit={this.formSubmit}>
+                    <label htmlFor="Name">Name: </label>     
+                    <input type="text" name="name" id="name" onChange={this.handleChange}/>
+                    <label htmlFor="age">Age: </label>
+                    <input type="text" name="age" id="age" onChange={this.handleChange}/>
+                    <label htmlFor="belt">Belt: </label>
+                    <input type="text" name="belt" id="belt" onChange={this.handleChange}/>
+                    <button>Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
